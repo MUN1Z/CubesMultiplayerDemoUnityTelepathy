b62936601e6f38bc4d6742519d133def2299f0d0
Mun1z <felipemunizicm2012@hotmail.com>
2018-10-21 01:30:19 -0300

    Creation of connection client server

---
+++ CubesMultiplayerDemoClient/Assets/Scripts/Network.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using Telepathy;
+using UnityEngine;
+
+public class Network : MonoBehaviour
+{
+    private Client client;
+    
+    // Use this for initialization
+    void Start () {
+        client = new Client();
+        client.Connect("127.0.0.1", 1337);
+
+        Thread.Sleep(15);
+
+        if (client.Connected)
+            Debug.Log("Client  started!");
+        else
+            Debug.LogError("Could not start client!");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- CubesMultiplayerDemoServer/src/Server/Program.cs
+++ CubesMultiplayerDemoServer/src/Server/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using Telepathy;
 
 namespace Server
 {
     class Program
     {
+        private Telepathy.Server server;
+        
+        public void Run()
+        {
+            try
+            {
+                server = new Telepathy.Server();
+                server.Start(1337);
+                
+                while (server.Active)
+                {
+                    // reply to each incoming message
+                    Message msg;
+                    while (server.GetNextMessage(out msg))
+                    {
+                        if (msg.eventType == EventType.Data)
+                        {
+                            server.Send(msg.connectionId, msg.data);
+                        }
+                    }
+
+                    System.Threading.Thread.Sleep(15);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            program.Run();
+
+            Console.ReadKey();
         }
     }
 }
